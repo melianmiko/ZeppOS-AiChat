769e48563ab1c6749df6add4322c276b6897105b
MelianMiko <melianmiko@gmail.com>
2023-07-30 13:27:25 +0700

    Add copyright

--- page/AboutScreen.js
+++ page/AboutScreen.js
@@ -4,26 +4,27 @@ import {gettext as t} from "i18n";
 import {VERSION} from "../version";
 
 class AboutScreen extends BaseAboutScreen {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.appId = FsTools.getAppTags()[0];
-    this.iconSize = 100;
-    this.appName = "AI-Chat";
-    this.version = VERSION;
+        this.appId = FsTools.getAppTags()[0];
+        this.iconSize = 100;
+        this.appName = "AI-Chat";
+        this.version = VERSION;
 
-    this.donateText = t("Donate");
-    this.donateUrl = "page/DonateScreen";
+        this.donateText = t("Donate");
+        this.donateUrl = "page/DonateScreen";
 
-    this.infoRows = [
-      ["melianmiko", "Developer"],
-      ["mmk.pw", "Website"],
-    ];
-  }
+        this.infoRows = [
+            ["melianmiko", "Developer"],
+            ["yarchefisʸᵗ", "Icon designer"],
+            ["mmk.pw", "Website"],
+        ];
+    }
 }
 
 Page({
-  onInit(p) {
-    new AboutScreen().start();
-  }
+    onInit(p) {
+        new AboutScreen().start();
+    }
 });
