e6d5b59a3e39a9a8db80a11536d18213f31e0d44
MelianMiko <melianmiko@gmail.com>
2023-09-01 16:47:02 +0700

    Update locale, add crowdin, fixes

--- setting/index.js
+++ setting/index.js
@@ -26,10 +26,10 @@ AppSettingsPage({
             Text({}, gettext("Like this application? Consider to support their development with a small donation: ")),
             Text(URL_TO_COPY, "https://mmk.pw/donate")
         ]),
-        // Paragraph2([
-        //     Text({}, gettext("Want to see AI-Chat interface into another language? You can help us with translation: ")),
-        //     Text(URL_TO_COPY, "https://crowdin.com/project/zeppchat")
-        // ]),
+        Paragraph2([
+            Text({}, gettext("Want to see AI-Chat interface into another language? You can help us with translation: ")),
+            Text(URL_TO_COPY, "https://crowdin.com/project/zeppchat")
+        ]),
     ]);
   },
 })
